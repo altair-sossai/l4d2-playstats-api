ec57544fc0a69d914799da30a1d1dff14c4e8d0d
Altair Sossai <altairsossai@gmail.com>
2023-02-09 11:53:33 -0300

    Update StatisticsResult

--- L4D2PlayStats.Core/Modules/Statistics/Results/StatisticsResult.cs
+++ L4D2PlayStats.Core/Modules/Statistics/Results/StatisticsResult.cs
@@ -4,5 +4,6 @@ public class StatisticsResult
 {
     public string? Server { get; set; }
     public string? FileName { get; set; }
+    public bool Completed => Statistic is { GameRound: { }, Halves.Count: 2, Scoring: { } };
     public L4D2PlayStats.Statistics? Statistic { get; set; }
 }
