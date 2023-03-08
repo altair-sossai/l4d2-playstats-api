856530636ac2f7e92e0678c5153fdb42d2e09ed0
Altair Sossai <altairsossai@gmail.com>
2023-03-08 18:18:26 -0300

    mixfix

--- L4D2PlayStats.Core/Modules/Mix/Services/IMixService.cs
+++ L4D2PlayStats.Core/Modules/Mix/Services/IMixService.cs
@@ -1,42 +1,9 @@
-﻿using FluentValidation;
-using L4D2PlayStats.Core.Modules.Mix.Commands;
+﻿using L4D2PlayStats.Core.Modules.Mix.Commands;
 using L4D2PlayStats.Core.Modules.Mix.Results;
-using L4D2PlayStats.Core.Modules.Ranking.Services;
 
 namespace L4D2PlayStats.Core.Modules.Mix.Services;
 
 public interface IMixService
 {
     Task<MixResult> MixAsync(string server, MixCommand command);
-}
-
-public class MixService : IMixService
-{
-    private readonly IRankingService _rankingService;
-    private readonly IValidator<MixCommand> _validator;
-
-    public MixService(IRankingService rankingService,
-        IValidator<MixCommand> validator)
-    {
-        _rankingService = rankingService;
-        _validator = validator;
-    }
-
-    public async Task<MixResult> MixAsync(string server, MixCommand command)
-    {
-        await _validator.ValidateAndThrowAsync(command);
-
-        var ranking = await _rankingService.RankingAsync(server);
-        var players = ranking.ToDictionary(k => k.CommunityId.ToString(), v => v);
-        var availables = command.All
-            .OrderBy(communityId => players.ContainsKey(communityId) ? players[communityId].Position : 0)
-            .ToList();
-
-        if (availables.Count != 8)
-            throw new Exception("It was not possible to generate the mix based on the data entered");
-
-        var result = new MixResult(availables, players);
-
-        return result;
-    }
 }
