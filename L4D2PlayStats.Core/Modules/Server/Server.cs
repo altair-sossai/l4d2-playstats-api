387e93a1fd5881f30b5999a3601076840d9521eb
Altair Sossai <altairsossai@gmail.com>
2023-03-09 08:59:15 -0300

    Added the ConfigurationName property in the Servers table

--- L4D2PlayStats.Core/Modules/Server/Server.cs
+++ L4D2PlayStats.Core/Modules/Server/Server.cs
@@ -5,6 +5,9 @@ namespace L4D2PlayStats.Core.Modules.Server;
 
 public class Server : ITableEntity
 {
+    private string? _configurationName;
+    private HashSet<string>? _configurationNames;
+
     public string Id
     {
         get => RowKey;
@@ -13,8 +16,27 @@ public class Server : ITableEntity
 
     public string? DisplayName { get; set; }
     public string? Secret { get; set; }
+
+    public string? ConfigurationName
+    {
+        get => _configurationName;
+        set
+        {
+            _configurationName = value;
+            _configurationNames = value?.Split(';').ToHashSet();
+        }
+    }
+
     public string PartitionKey { get; set; } = "shared";
     public string RowKey { get; set; } = default!;
     public DateTimeOffset? Timestamp { get; set; }
     public ETag ETag { get; set; }
+
+    public bool RankingConfiguration(string? configurationName)
+    {
+        return !string.IsNullOrEmpty(configurationName)
+               && _configurationNames != null
+               && _configurationNames.Count != 0
+               && _configurationNames.Contains(configurationName);
+    }
 }
