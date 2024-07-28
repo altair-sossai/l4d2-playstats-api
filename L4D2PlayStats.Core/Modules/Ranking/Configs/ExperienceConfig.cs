3d8e76d96dc2fff670e2f1e7905fa48cee48423d
Altair Sossai <altairsossai@gmail.com>
2024-07-28 08:37:53 -0300

    Refactor ExperienceConfig for dependency injection
    Refactored the `ExperienceConfig` class to use dependency injection for configuration settings. The `IConfiguration` interface from `Microsoft.Extensions.Configuration` is now used to retrieve configuration values. Updated the class definition to accept an `IConfiguration` object in the constructor. Replaced hardcoded values for `DaysRange`, `Win`, `Loss`, `Mvps`, and `MvpsCommon` with calls to `configuration.GetValue`. Moved the namespace declaration to follow the `using` directive.

--- L4D2PlayStats.Core/Modules/Ranking/Configs/ExperienceConfig.cs
+++ L4D2PlayStats.Core/Modules/Ranking/Configs/ExperienceConfig.cs
@@ -1,10 +1,12 @@
-﻿namespace L4D2PlayStats.Core.Modules.Ranking.Configs;
+﻿using Microsoft.Extensions.Configuration;
 
-public class ExperienceConfig : IExperienceConfig
+namespace L4D2PlayStats.Core.Modules.Ranking.Configs;
+
+public class ExperienceConfig(IConfiguration configuration) : IExperienceConfig
 {
-    public int DaysRange => 60;
-    public int Win => 80;
-    public int Loss => -65;
-    public int Mvps => 7;
-    public int MvpsCommon => 5;
+    public int DaysRange => configuration.GetValue("ExperienceConfig:DaysRange", 60);
+    public int Win => configuration.GetValue("ExperienceConfig:Win", 80);
+    public int Loss => configuration.GetValue("ExperienceConfig:Loss", -65);
+    public int Mvps => configuration.GetValue("ExperienceConfig:Mvps", 7);
+    public int MvpsCommon => configuration.GetValue("ExperienceConfig:MvpsCommon", 5);
 }
