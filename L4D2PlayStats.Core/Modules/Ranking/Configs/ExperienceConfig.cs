739d8bbf76d569024dcb9ef9240d67e276e5a259
Altair Sossai <altairsossai@gmail.com>
2024-11-01 08:29:56 -0300

    Update ExperienceConfig default values for rebalancing
    Adjusted default experience points in ExperienceConfig:
    - Increased `Win` from 80 to 85
    - Decreased `Loss` from -65 to -75
    - Decreased `RageQuit` from -130 to -150
    - Increased `Mvps` from 7 to 8
    - Increased `MvpsCommon` from 5 to 6
    
    These changes aim to better align experience points with the desired gameplay experience.

--- L4D2PlayStats.Core/Modules/Ranking/Configs/ExperienceConfig.cs
+++ L4D2PlayStats.Core/Modules/Ranking/Configs/ExperienceConfig.cs
@@ -4,9 +4,9 @@ namespace L4D2PlayStats.Core.Modules.Ranking.Configs;
 
 public class ExperienceConfig(IConfiguration configuration) : IExperienceConfig
 {
-    public int Win => configuration.GetValue("ExperienceConfig:Win", 80);
-    public int Loss => configuration.GetValue("ExperienceConfig:Loss", -65);
-    public int RageQuit => configuration.GetValue("ExperienceConfig:RageQuit", -130);
-    public int Mvps => configuration.GetValue("ExperienceConfig:Mvps", 7);
-    public int MvpsCommon => configuration.GetValue("ExperienceConfig:MvpsCommon", 5);
+    public int Win => configuration.GetValue("ExperienceConfig:Win", 85);
+    public int Loss => configuration.GetValue("ExperienceConfig:Loss", -75);
+    public int RageQuit => configuration.GetValue("ExperienceConfig:RageQuit", -150);
+    public int Mvps => configuration.GetValue("ExperienceConfig:Mvps", 8);
+    public int MvpsCommon => configuration.GetValue("ExperienceConfig:MvpsCommon", 6);
 }
