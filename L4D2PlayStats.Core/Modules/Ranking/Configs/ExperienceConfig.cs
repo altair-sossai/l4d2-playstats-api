5d5725592ab4386eafda8c7b7624d1ff2da5159d
Altair Sossai <altairsossai@gmail.com>
2025-01-02 13:57:22 -0300

    Update Loss experience configuration value
    Changed the Loss value in the experience configuration from -75 to -80.

--- L4D2PlayStats.Core/Modules/Ranking/Configs/ExperienceConfig.cs
+++ L4D2PlayStats.Core/Modules/Ranking/Configs/ExperienceConfig.cs
@@ -5,7 +5,7 @@ namespace L4D2PlayStats.Core.Modules.Ranking.Configs;
 public class ExperienceConfig(IConfiguration configuration) : IExperienceConfig
 {
     public int Win => configuration.GetValue("ExperienceConfig:Win", 85);
-    public int Loss => configuration.GetValue("ExperienceConfig:Loss", -75);
+    public int Loss => configuration.GetValue("ExperienceConfig:Loss", -80);
     public int RageQuit => configuration.GetValue("ExperienceConfig:RageQuit", -150);
     public int Mvps => configuration.GetValue("ExperienceConfig:Mvps", 8);
     public int MvpsCommon => configuration.GetValue("ExperienceConfig:MvpsCommon", 6);
