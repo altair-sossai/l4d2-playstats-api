59797b5f374892bc95e3aea148a60e62fa95ed29
Altair Sossai <altairsossai@gmail.com>
2025-07-03 17:12:11 -0300

    Add support for bimonthly and annual file patterns
    Updated the `HistoryModel` class to handle two file naming patterns: bimonthly and annual. Retained the original regex for bimonthly files and added a new regex for annual files. Introduced `IsBimonthly` and `IsAnnual` properties to check file name validity. Modified the `_fileName` setter to parse dates based on the file type and updated the static `Parse` method to return a `HistoryModel` instance for valid file names.

--- L4D2PlayStats.Core/Modules/Ranking/Model/HistoryModel.cs
+++ L4D2PlayStats.Core/Modules/Ranking/Model/HistoryModel.cs
@@ -4,8 +4,12 @@ namespace L4D2PlayStats.Core.Modules.Ranking.Model;
 
 public class HistoryModel
 {
-    private const string Pattern = @"^ranking_(\d{4})(\d{2})(\d{4})(\d{2})\.json$";
-    private static readonly Regex Regex = new(Pattern);
+    private const string BimonthlyPattern = @"^ranking_(\d{4})(\d{2})(\d{4})(\d{2})\.json$";
+    private const string AnnualPattern = @"^ranking_(\d{4})\.json$";
+
+    private static readonly Regex BimonthlyRegex = new(BimonthlyPattern);
+    private static readonly Regex AnnualRegex = new(AnnualPattern);
+
     private string _fileName = null!;
 
     public HistoryModel(string fileName)
@@ -22,13 +26,25 @@ public class HistoryModel
         {
             _fileName = value;
 
-            var match = Regex.Match(value);
+            if (IsBimonthly)
+            {
+                var match = BimonthlyRegex.Match(value);
 
-            StartYear = int.Parse(match.Groups[1].Value);
-            StartMonth = int.Parse(match.Groups[2].Value);
+                StartYear = int.Parse(match.Groups[1].Value);
+                StartMonth = int.Parse(match.Groups[2].Value);
 
-            EndYear = int.Parse(match.Groups[3].Value);
-            EndMonth = int.Parse(match.Groups[4].Value);
+                EndYear = int.Parse(match.Groups[3].Value);
+                EndMonth = int.Parse(match.Groups[4].Value);
+            }
+
+            if (IsAnnual)
+            {
+                var match = AnnualRegex.Match(value);
+                StartYear = int.Parse(match.Groups[1].Value);
+                StartMonth = 1;
+                EndYear = StartYear;
+                EndMonth = 12;
+            }
         }
     }
 
@@ -37,8 +53,11 @@ public class HistoryModel
     public int EndYear { get; private set; }
     public int EndMonth { get; private set; }
 
+    public bool IsBimonthly => BimonthlyRegex.IsMatch(FileName);
+    public bool IsAnnual => AnnualRegex.IsMatch(FileName);
+
     public static HistoryModel? Parse(string fileName)
     {
-        return Regex.IsMatch(fileName) ? new HistoryModel(fileName) : null;
+        return BimonthlyRegex.IsMatch(fileName) || AnnualRegex.IsMatch(fileName) ? new HistoryModel(fileName) : null;
     }
 }
